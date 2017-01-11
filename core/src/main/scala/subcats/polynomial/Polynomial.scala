3902d5ecfac2973e0ba3a7c9da0df3766ff56d38
Alexander Konovalov <alex.knvl@gmail.com>
2017-01-11 17:07:19 -0500

    Fixed indentation, added a simplified version of polynomial functors for clarity.

--- core/src/main/scala/subcats/polynomial/Polynomial.scala
+++ core/src/main/scala/subcats/polynomial/Polynomial.scala
@@ -4,62 +4,78 @@ import cats.Functor
 import simulacrum.typeclass
 
 object Polynomial {
-sealed abstract class TypeF protected() {
-  type F[A]
-  def F: Functor[F]
-}
-object TypeF {
-  sealed abstract class Aux[F_[_]] extends TypeF {
-    type F[A] = F_[A]
+
+  /**
+    * In essence: {{{
+    *   sealed abstract class TypeF protected() { type F[A] }
+    *   object TypeF {
+    *     sealed abstract class Aux[F_[_]] extends TypeF { type F[A] = F_[A] }
+    *     final class I extends Aux[λ[X => X]]
+    *     final class K[A] extends Aux[λ[X => A]]
+    *     final class :+:[A <: TypeF, B <: TypeF] extends Aux[λ[X => Either[A#F[X], B#F[X]]]]
+    *     final class :*:[A <: TypeF, B <: TypeF] extends Aux[λ[X => (A#F[X], B#F[X])]]
+    *   }
+    *   import TypeF._
+    *   type ListF[A, B]  = (K[Unit] :+: (K[A] :*: I))#F[B]
+    * }}}
+    */
+  
+  sealed abstract class TypeF protected() {
+    type F[A]
     def F: Functor[F]
   }
-
-  implicit class TypeFOps[T <: TypeF](t: T) {
-    def :+:[U <: TypeF](u: U): :+:[U, T] = new :+:(u, t)
-    def :*:[U <: TypeF](u: U): :*:[U, T] = new :*:(u, t)
-  }
-
-  final class I extends Aux[λ[X => X]] {
-    val F = new Functor[λ[X => X]] {
-      override def map[A, B](a: A)(f: (A) => B): B = f(a)
+  object TypeF {
+    sealed abstract class Aux[F_[_]] extends TypeF {
+      type F[A] = F_[A]
+      def F: Functor[F]
     }
-  }
-  def I: I = new I()
 
-  final class K[A] extends Aux[λ[X => A]] {
-    val F = new Functor[λ[X => A]] {
-      override def map[X, Y](a: A)(f: X => Y): A = a
+    implicit class TypeFOps[T <: TypeF](t: T) {
+      def :+:[U <: TypeF](u: U): :+:[U, T] = new :+:(u, t)
+      def :*:[U <: TypeF](u: U): :*:[U, T] = new :*:(u, t)
     }
-  }
-  def K[A]: K[A] = new K[A]()
 
-  final class :+:[A <: TypeF, B <: TypeF](A: A, B: B) extends Aux[λ[X => Either[A#F[X], B#F[X]]]] {
-    val AF: Functor[A#F] = A.F.asInstanceOf[Functor[A#F]]
-    val BF: Functor[B#F] = B.F.asInstanceOf[Functor[B#F]]
+    final class I extends Aux[λ[X => X]] {
+      val F = new Functor[λ[X => X]] {
+        override def map[A, B](a: A)(f: (A) => B): B = f(a)
+      }
+    }
+    def I: I = new I()
 
-    val F = new Functor[λ[X => Either[A#F[X], B#F[X]]]] {
-      override def map[X, Y](v: Either[A#F[X], B#F[X]])(f: (X) => Y): Either[A#F[Y], B#F[Y]] = v match {
-        case Left(a) => Left(AF.map(a)(f))
-        case Right(b) => Right(BF.map(b)(f))
+    final class K[A] extends Aux[λ[X => A]] {
+      val F = new Functor[λ[X => A]] {
+        override def map[X, Y](a: A)(f: X => Y): A = a
+      }
+    }
+    def K[A]: K[A] = new K[A]()
+
+    final class :+:[A <: TypeF, B <: TypeF](A: A, B: B) extends Aux[λ[X => Either[A#F[X], B#F[X]]]] {
+      val AF: Functor[A#F] = A.F.asInstanceOf[Functor[A#F]]
+      val BF: Functor[B#F] = B.F.asInstanceOf[Functor[B#F]]
+
+      val F = new Functor[λ[X => Either[A#F[X], B#F[X]]]] {
+        override def map[X, Y](v: Either[A#F[X], B#F[X]])(f: (X) => Y): Either[A#F[Y], B#F[Y]] = v match {
+          case Left(a) => Left(AF.map(a)(f))
+          case Right(b) => Right(BF.map(b)(f))
+        }
+      }
+    }
+
+    final class :*:[A <: TypeF, B <: TypeF](A: A, B: B) extends Aux[λ[X => (A#F[X], B#F[X])]] {
+      val AF: Functor[A#F] = A.F.asInstanceOf[Functor[A#F]]
+      val BF: Functor[B#F] = B.F.asInstanceOf[Functor[B#F]]
+
+      val F = new Functor[λ[X => (A#F[X], B#F[X])]] {
+        override def map[X, Y](v: (A#F[X], B#F[X]))(f: (X) => Y): (A#F[Y], B#F[Y]) = v match {
+          case (a, b) => (AF.map(a)(f), BF.map(b)(f))
+        }
       }
     }
   }
 
-  final class :*:[A <: TypeF, B <: TypeF](A: A, B: B) extends Aux[λ[X => (A#F[X], B#F[X])]] {
-    val AF: Functor[A#F] = A.F.asInstanceOf[Functor[A#F]]
-    val BF: Functor[B#F] = B.F.asInstanceOf[Functor[B#F]]
-
-    val F = new Functor[λ[X => (A#F[X], B#F[X])]] {
-      override def map[X, Y](v: (A#F[X], B#F[X]))(f: (X) => Y): (A#F[Y], B#F[Y]) = v match {
-        case (a, b) => (AF.map(a)(f), BF.map(b)(f))
-      }
-    }
-  }
-}
-
-import TypeF._
-type                           ListF[A, B]  = (K[Unit] :+: (K[A] :*: I))#F[B]
-implicit def ListF[A]: Functor[ListF[A, ?]] = (K[Unit] :+: (K[A] :*: I)).F
+  import TypeF._
+  type                           ListF[A, B]  = (K[Unit] :+: (K[A] :*: I))#F[B]
+  implicit def ListF[A]: Functor[ListF[A, ?]] = (K[Unit] :+: (K[A] :*: I)).F
 
   type Algebra[F[_], A] = F[A] => A
   type Coalgebra[F[_], A] = A => F[A]
